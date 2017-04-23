2b187a1083dc491540a53edbc935fdb547cc0ffe
Ronoel <ronoeljr@gmail.com>
2017-04-23 10:18:50 -0300

    - Added CORS

--- src/main/java/com/ronoel/decorateste/resource/ApplicationConfig.java
+++ src/main/java/com/ronoel/decorateste/resource/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.ronoel.decorateste.resource.CrossOriginResourceSharingFilter.class);
         resources.add(com.ronoel.decorateste.resource.UserResource.class);
         resources.add(com.ronoel.decorateste.resource.exception.UserExistsExceptionWeb.class);
         resources.add(com.ronoel.decorateste.resource.exception.UserNotExistExceptionWeb.class);
